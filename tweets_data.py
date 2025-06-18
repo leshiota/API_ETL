b6b6f71bb4365c1079197ae6dfb1b8feb41b814e
Your Name <you@example.com>
2025-06-18 15:48:25 -0300

    add the flow

--- tweets_data.py
+++ tweets_data.py
@@ -14,6 +14,8 @@ user_db = os.getenv("user_db")
 passwd_db = os.getenv("passwd_db")
 search_query = '"Mercado livre" "Frete grátis" "R$19" -is:retweet -is:reply -has:links'
 number_of_tweets = 100
+pg_user = os.getenv("user_db")
+pg_password = os.getenv("passwd_db")
 
 
 @task(name ='token_connection')
@@ -21,24 +23,25 @@ def connection(bearer_token: str):
     """Create the connection with X API"""
     return tweepy.Client(bearer_token, return_type = requests.Response)
 
-def tweet_data(query: str, max_results: int = 100):
-    """Fetch tweets using the X API."""
-    response = client.search_recent_tweets(
-        query=query,
-        tweet_fields=["created_at", "public_metrics", "author_id","text"]
-        max_results=max_results
-    )
-    tweets = response.data or []
-    return [
-        {
-            "id": tweet.id,
-            "created_at": tweet.created_at,
-            "author_id": tweet.author_id,
-            "likes" : tweet.public_metrics["like_count"],
-            "text": tweet.text
-        }
-        for tweet in tweets
-    ]
+#@task(name='tweet_data')
+#def tweet_data(query: str, max_results: int = 100, client):
+#    """Fetch tweets using the X API."""
+#    response = client.search_recent_tweets(
+#        query=query,
+#        tweet_fields=["created_at", "public_metrics", "author_id","text"]
+#        max_results=max_results
+#    )
+#    tweets = response.data or []
+#    return [
+#        {
+#            "id": tweet.id,
+#            "created_at": tweet.created_at,
+#            "author_id": tweet.author_id,
+#            "likes" : tweet.public_metrics["like_count"],
+#            "text": tweet.text
+#        }
+#        for tweet in tweets
+#    ]
 
 
 @task
@@ -50,7 +53,7 @@ def conect_database(pg_user: str,pg_password: str):
     return mysql.connector.connect(
         host = {host_db},
         user = {user_db},
-        passwd = {passwd_db}
+        passwd = {pg_password},
         auth_plugin = "mysql_native_password")
 
 @task
@@ -70,7 +73,7 @@ def create_table(mydb: str):
 
 
 @task
-def tweets_information(number_of_tweets, search_query, mydb):
+def tweets_information(number_of_tweets, search_query, mydb,client):
     try:
         response = client.search_recent_tweets(
             query=search_query,
@@ -98,15 +101,26 @@ def tweets_information(number_of_tweets, search_query, mydb):
 @task
 def save_database(mybd):
     """Insert X data into database"""
+    my_cursor = mydb.cursor()
+    my_cursor.execute('INSERT INTO tweet_data_meli (tweeter_id, created_at, author_id, likes, text) VALUES (id, created_at, author_id, likes, text)')
+    mydb.commit()
+    mydb.close()
     
 
 
 @flow(name='Twitter_ETL')
 def twitter_etl():
+    client = connection(bearer_token)
+    mydb = conect_database(pg_user,pg_password)
+    database = create_database(mydb,'tweets_data')
+    table = create_table(mydb)
+    data_api = tweets_information(search_query, number_of_tweets,client)
+
+
 
 
 if __name__ == "__main__":
     twitter_etl()
-
+    
 
 
